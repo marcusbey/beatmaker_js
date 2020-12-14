408c61e036ed7c5af674ad5a17da000bcba603c2
Romain BOBOE <rboboe@gmail.com>
2020-12-14 14:14:38 -0500

    creating the loop for the active pads

--- app.js
+++ app.js
@@ -8,16 +8,25 @@ class Drumkit {
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.index = 0;
-        this.bpm = 100;
+        this.bpm = 200;
     }
 
+    //get the active pad 
+    activePad() {
+        this.classList.toggle("active");
+    }
     repeat() {
         const step = this.index % 10;
-        const activeBars = document.querySelector(`.b${step}`);
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        //loop over the pads 
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            console.log(bar);
+        });
         this.index ++;
         console.log(step);
     }
-
+  
     start() {
         const interval = (60 / this.bpm) * 1000;
         setInterval(()=>{
@@ -27,6 +36,14 @@ class Drumkit {
     }
 
 const drumKit = new Drumkit();
+
+drumKit.pads.forEach(pad => {
+    pad.addEventListener("click", drumKit.activePad);
+    pad.addEventListener('animationend', function(){
+        this.style.animation = "";
+    })
+})
+
 
 drumKit.playSound.addEventListener("click", function(){
     drumKit.start();
