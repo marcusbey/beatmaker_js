a8830a77c6e402050392eeca5d709f3af3c9a168
Romain BOBOE <rboboe@gmail.com>
2020-12-15 20:43:47 -0500

    adding a reset button

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ class Drumkit {
         this.selects = document.querySelectorAll('.select');
         this.muteBtns = document.querySelectorAll('.mute');
         this.tempoSlider = document.querySelector('.tempo-slider');
+        this.resetBtn = document.querySelector('.resetBtn');
     }
 
     //get the active pad 
@@ -174,6 +175,12 @@ class Drumkit {
             this.start();
         }
     }
+
+    reset(){
+        this.pads.forEach(pad => {
+            pad.classList.remove('active');
+        })
+    }
 }
 
 
@@ -190,6 +197,11 @@ drumKit.pads.forEach(pad => {
 drumKit.playBtn.addEventListener("click", function(){
     drumKit.updateBtn();
     drumKit.start();
+});
+
+
+drumKit.resetBtn.addEventListener("click", function(){
+    drumKit.reset();
 });
 
 
